6dc7d131ca9e0773033bb61823ca16220a7e4e38
viqofirdaus <viqo.firdaus@gmail.com>
2026-05-28 01:03:25 +0800

    Implement distribution update functionality and enhance PengirimanPage UI

---
+++ actions/distributions.ts
@@ -0,0 +1,36 @@
+import client from '@/lib/mongodb';
+import { Distribution, DistributionStatus } from '@/types';
+import { ObjectId } from 'mongodb';
+import { revalidatePath } from 'next/cache';
+
+type DistributionParams = { id: string; status: DistributionStatus };
+
+export async function updateDistribution({ id, status }: DistributionParams) {
+  const db = client.db('kurban1447h');
+  const query = { _id: new ObjectId(id) };
+
+  const now = new Date().toISOString();
+
+  const nextStatus = !status ? 'otw' : status === 'otw' ? 'finish' : null;
+  const updateSet: Partial<Distribution> = { status: nextStatus };
+
+  if (nextStatus === 'otw') {
+    updateSet.timestampStart = now;
+  }
+
+  if (nextStatus === 'finish') {
+    updateSet.timestampFin = now;
+  }
+
+  // Reset everything
+  if (nextStatus === null) {
+    updateSet.timestampStart = null;
+    updateSet.timestampFin = null;
+  }
+
+  await db.collection('distributions').updateOne(query, {
+    $set: updateSet,
+  });
+
+  revalidatePath('/pengiriman');
+}
--- app/pengiriman/page.tsx
+++ app/pengiriman/page.tsx
@@ -1,10 +1,90 @@
-import { PageTitle } from '@/components';
-import { WrapperMain } from '@/components/wrapper';
+import { updateDistribution } from '@/actions/distributions';
+import { Button, PageTitle } from '@/components';
+import { Card } from '@/components/card';
+import { WrapperCard, WrapperMain } from '@/components/wrapper';
+import client from '@/lib/mongodb';
+import { Distribution } from '@/types';
+import { toTwentyFourHours } from '@/utils/toTwentyFourHours';
+import { CheckIcon, HouseIcon, SchoolIcon, TruckIcon } from 'lucide-react';
 
-export default function PengirimanPage() {
+export default async function PengirimanPage() {
+  const db = client.db('kurban1447h');
+  const dist = await db
+    .collection<Distribution>('distributions')
+    .find({})
+    .sort({ timestamp: -1 })
+    .toArray();
+
   return (
     <WrapperMain>
       <PageTitle />
+      <WrapperCard className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+        {dist.map((trans) => {
+          const status = trans.status;
+          const buttonStr = !status
+            ? 'Berangkat!'
+            : status === 'otw'
+              ? 'Sampai!'
+              : 'Reset';
+
+          return (
+            <Card key={trans._id.toString()}>
+              <div className='mb-4 flex items-center justify-between'>
+                <h2 className='text-3xl'>RT {trans.rt}</h2>
+                {status === 'finish' && (
+                  <CheckIcon size={30} className='text-ctp-green' />
+                )}
+              </div>
+              <div className='flex flex-col gap-2'>
+                <div className='flex justify-between'>
+                  {status && <SchoolIcon size={36} />}
+                  <TruckIcon
+                    size={36}
+                    className={`${status === 'otw' ? 'animate-bounce-tilt' : ''}`}
+                  />
+                  {status !== 'finish' && <HouseIcon size={36} />}
+                </div>
+                <div className='flex items-center justify-between px-4'>
+                  <div className='aspect-square h-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='aspect-square h-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='h-0.5 w-2 rounded-full bg-black' />
+                  <div className='aspect-square h-2 rounded-full bg-black' />
+                </div>
+                <div className='flex items-center justify-between px-1'>
+                  {trans.timestampStart && (
+                    <span className='text-sm'>
+                      {toTwentyFourHours(trans.timestampStart)}
+                    </span>
+                  )}
+                  {trans.timestampFin && (
+                    <span className='text-sm'>
+                      {toTwentyFourHours(trans.timestampFin)}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <form
+                key={trans._id.toString() + 'form'}
+                className='flex'
+                action={async () => {
+                  'use server';
+                  await updateDistribution({
+                    id: trans._id.toString(),
+                    status,
+                  });
+                }}
+              >
+                <Button className='mt-4 mr-auto ml-auto'>{buttonStr}</Button>
+              </form>
+            </Card>
+          );
+        })}
+      </WrapperCard>
     </WrapperMain>
   );
 }
