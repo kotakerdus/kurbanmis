3ede273f5b6260fa3f6061ed09bb7f0149668963
viqofirdaus <viqo.firdaus@gmail.com>
2026-05-27 22:56:33 +0800

    UI update on Button

--- components/button.tsx
+++ components/button.tsx
@@ -3,7 +3,7 @@ export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={`border-ctp-text/20 hover:text-ctp-base hover:bg-accent cursor-pointer rounded-md border px-2 py-1 ${props.className}`}
+      className={`border-ctp-text/20 hover:text-ctp-base disabled:text-ctp-text/80 not-disabled:hover:bg-accent disabled:hover:bg-ctp-base w-full cursor-pointer rounded-md border px-2 py-1 disabled:border-dashed ${props.className}`}
     >
       {children}
     </button>
