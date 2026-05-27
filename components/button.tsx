c84a4bb4451a26476d4e60fd63148605ef636d43
viqofirdaus <viqo.firdaus@gmail.com>
2026-05-27 22:15:00 +0800

    Fixed Button type

--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,9 @@
-type ButtonProps = React.HtmlHTMLAttributes<HTMLButtonElement>;
-
-export function Button({ className, children, ...props }: ButtonProps) {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {};
+export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={`border-ctp-text/20 hover:text-ctp-base hover:bg-accent cursor-pointer rounded-md border px-2 py-1 ${className}`}
+      className={`border-ctp-text/20 hover:text-ctp-base hover:bg-accent cursor-pointer rounded-md border px-2 py-1 ${props.className}`}
     >
       {children}
     </button>
