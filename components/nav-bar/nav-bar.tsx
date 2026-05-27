b3902336bd80264f688ace167b8847e47d00922a
kotakerdus <viqo.firdaus@gmail.com>
2026-05-28 07:28:03 +0800

    Made NavBar hidden at start

--- components/nav-bar/nav-bar.tsx
+++ components/nav-bar/nav-bar.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { NavLink } from '.';
 
 export function NavBar() {
-  const [hide, setHide] = useState(false);
+  const [hide, setHide] = useState(true);
 
   return (
     <>
