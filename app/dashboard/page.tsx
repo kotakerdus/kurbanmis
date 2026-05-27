616a218f92891e61b2592326f6e56fcca4211c79
viqofirdaus <viqo.firdaus@gmail.com>
2026-05-28 01:45:34 +0800

    Add distribution list to DashboardPage and update UI for status display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,9 @@ import { Card, CardNumber } from '@/components/card';
 import { ChartProgress } from '@/components/chart';
 import { WrapperCard, WrapperMain } from '@/components/wrapper';
 import client from '@/lib/mongodb';
-import { Log, LogType, QurbanList } from '@/types';
+import { Distribution, Log, LogType, QurbanList } from '@/types';
 import { relativeTime } from '@/utils/relativeTime';
+import { CheckIcon } from 'lucide-react';
 
 export default async function DashboardPage() {
   const db = client.db('kurban1447h');
@@ -17,6 +18,10 @@ export default async function DashboardPage() {
     .find({})
     .limit(20)
     .sort({ timestamp: -1 })
+    .toArray();
+  const distributons = await db
+    .collection<Distribution>('distributions')
+    .find({})
     .toArray();
 
   const totalLimoSl = data.filter(
@@ -57,38 +62,69 @@ export default async function DashboardPage() {
           />
         </div>
       </Card>
-      <Card title='Update Terkini 🔥'>
-        <div className='relative'>
-          <div className='absolute top-0 h-5 w-full bg-linear-to-b from-white to-transparent' />
-          <div className='absolute bottom-0 h-5 w-full bg-linear-to-t from-white to-transparent' />
-          <ul className='divide-ctp-text/20 h-70 space-y-2 divide-y overflow-scroll'>
-            {logs.map((log) => {
-              const tagColor: Record<LogType, string> = {
-                distribusi: 'bg-ctp-blue',
-                pengambilan: 'bg-ctp-green',
-                penyembelihan: 'bg-ctp-red',
-              };
+      <WrapperCard className='grid grid-cols-3 grid-rows-2 lg:grid-cols-4'>
+        <Card title='Distribusi 🚛' className='col-span-1'>
+          <ul className='divide-ctp-text/20 space-y-2 divide-y'>
+            {distributons.map((dist) => {
+              const id = dist._id.toString();
+              const status = dist.status;
+
+              const statusText = !status
+                ? 'Standby'
+                : status === 'otw'
+                  ? 'OTW'
+                  : 'Terkirim';
 
               return (
                 <li
-                  key={log.timestamp}
-                  className='not-last:pb-2 first:pt-2 last:pb-2'
+                  key={id}
+                  className='flex justify-between not-last:pb-2 first:pt-2 last:pb-2'
                 >
-                  <div className='flex items-center gap-3'>
-                    <div
-                      className={`h-3 w-3 rounded-full ${tagColor[log.type]}`}
-                    />
-                    <span className='font-mono text-sm text-gray-500 select-text'>
-                      {relativeTime(log.timestamp)}
-                    </span>
-                    <p className='flex-1 select-text'>{log.text}</p>
+                  <p>RT {dist.rt}</p>
+                  <div className='flex gap-2'>
+                    <p>{statusText}</p>
+                    {status === 'finish' && (
+                      <CheckIcon size={20} className='text-ctp-green' />
+                    )}
                   </div>
                 </li>
               );
             })}
           </ul>
-        </div>
-      </Card>
+        </Card>
+        <Card title='Update Terkini 🔥' className='col-span-2'>
+          <div className='relative'>
+            <div className='dark:from-background absolute top-0 h-5 w-full bg-linear-to-b from-white to-transparent' />
+            <div className='dark:from-background absolute bottom-0 h-5 w-full bg-linear-to-t from-white to-transparent' />
+            <ul className='divide-ctp-text/20 h-70 space-y-2 divide-y overflow-scroll'>
+              {logs.map((log) => {
+                const tagColor: Record<LogType, string> = {
+                  distribusi: 'bg-ctp-blue',
+                  pengambilan: 'bg-ctp-green',
+                  penyembelihan: 'bg-ctp-red',
+                };
+
+                return (
+                  <li
+                    key={log.timestamp}
+                    className='not-last:pb-2 first:pt-2 last:pb-2'
+                  >
+                    <div className='flex items-center gap-3'>
+                      <div
+                        className={`h-3 w-3 rounded-full ${tagColor[log.type]}`}
+                      />
+                      <span className='font-mono text-sm text-gray-500 select-text'>
+                        {relativeTime(log.timestamp)}
+                      </span>
+                      <p className='flex-1 select-text'>{log.text}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </Card>
+      </WrapperCard>
     </WrapperMain>
   );
 }
