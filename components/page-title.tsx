6758360f65fbc5d5a69ea6558af1d7f82b22c75b
viqofirdaus <viqo.firdaus@gmail.com>
2026-05-27 14:50:37 +0800

    Quick code update

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -8,7 +8,7 @@ export default function PageTitle({ title }: PageTitleProps) {
   const path = toTitleCase(usePathname().split('/')[1]);
   return (
     <div className='mb-2 flex flex-col gap-1'>
-      <h1 className='text-3xl font-bold'>{title ?? path}</h1>
+      <h1 className='text-3xl font-bold'>{title || path}</h1>
       <p className='text-ctp-subtext0 text-sm'>
         {"Kurban Masjid Imam Syafi'i Banjarmasin 1447H"}
       </p>
